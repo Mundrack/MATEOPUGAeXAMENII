26dae24fa6da410c249277c41148bf4902c13223
Mundrack <mateopuga75@gmail.com>
2024-10-28 10:10:36 -0500

    Agregar archivos de proyecto.

---
+++ MATEOPUGAeXAMENII/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MATEOPUGAeXAMENII.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
